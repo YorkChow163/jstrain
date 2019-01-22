84012dec8b99f9c4acfd4353f0f967dfc1362d6c
yorkchow163 <2171677178@qq.com>
2019-01-22 17:57:24 +0800

    字符串模板

--- es6test/zifukuozhan.js
+++ es6test/zifukuozhan.js
@@ -2,11 +2,12 @@
  * 字符串模板可以套用函数
  * @returns {string}
  */
-function fun2() {
+function fun2(a) {
+    if(!a){
+        return 'hehehe'
+    }
     return 'hahhaha';
 }
 
 let a=`foo function is ${fun2()}`
 console.log(a)
-
-
